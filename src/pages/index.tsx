4fd87fcd38a89286061d30b179305ac56576ebd3
yui162205 <s1f102401371@iniad.org>
2024-05-07 17:07:55 +0900

    色を変えるところまで

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,66 @@
 import { useState } from 'react';
 import styles from './index.module.css';
 
+const directions = [
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+  [-1, 0],
+];
+
 const Home = () => {
   const [turnColor, setTurnColor] = useState(1);
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [2, 2, 1, 2, 1, 2, 1, 0],
-    [1, 1, 2, 1, 2, 1, 2, 0],
-    [2, 2, 1, 2, 1, 2, 1, 0],
-    [1, 1, 2, 1, 2, 1, 2, 0],
-    [2, 2, 1, 2, 1, 2, 1, 0],
-    [1, 1, 2, 1, 2, 1, 2, 0],
-    [2, 1, 2, 1, 2, 1, 2, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 2, 0, 0, 0],
+    [0, 0, 0, 2, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+
   const clickHandler = (x: number, y: number) => {
     console.log(x, y);
     const newBoard = structuredClone(board);
-    for (let distance = 1; distance < 8; distance++) {
-      if (newBoard[y + distance] === undefined) {
-        break;
-      } else {
-        if (newBoard[y + distance] === undefined) {
+    for (let n: number = 0; n < 8; n++) {
+      const maybeTurnables: { x: number; y: number }[] = [];
+      const [dx, dy] = directions[n];
+      for (let i: number = 1; i < 8; i++) {
+        if (board[y + i * dy] === undefined) {
           break;
-        } else if (newBoard[y + distance][x] === 0) {
+        }
+        const checkingColor = board[y + i * dy][x + i * dx];
+        if (checkingColor === undefined) {
           break;
-        } else if (newBoard[y + distance][x] === turnColor) {
-          if (distance > 1) {
-            for (let back = distance; back >= 0; back--) {
-              newBoard[y + back][x] = turnColor;
-            }
-            setBoard(newBoard);
-            setTurnColor(3 - turnColor);
+        } else if (checkingColor === 0) {
+          break;
+        } else if (checkingColor === turnColor && i !== 1) {
+          // if (maybeTurnables.length > 0) {
+          for (const item of maybeTurnables) {
+            console.log(item);
+            console.log(maybeTurnables);
+            newBoard[item.y][item.x] = turnColor;
           }
+          newBoard[y][x] = turnColor;
+          setBoard(newBoard);
+          setTurnColor(3 - turnColor);
+          // }
           break;
-        } else if (newBoard[y + distance][x] === 3 - turnColor) {
-          continue;
+        } else if (checkingColor === 3 - turnColor) {
+          if (maybeTurnables.length > 0) {
+            maybeTurnables.push({ x: x + i * dx, y: y + i * dy });
+          }
         }
       }
     }
-    setBoard(newBoard);
   };
+
   return (
     <div className={styles.container}>
       <div className={styles.boardStyle}>
